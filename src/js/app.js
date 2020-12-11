c12addd7094290a06edddf37b79450d4a12bd14e
Ksenia Sidorova <nekfi@mail.ru>
2020-12-11 22:47:46 +0300

    Делаю сервер

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,13 @@
-/* eslint import/no-cycle:0 */ // --> OFF
-/* eslint  import/prefer-default-export:0 */ // --> OFF
-
-import start from './start';
-import next from './next';
-
-start();
-export const intervalID = setInterval(next, 1000);
+const form = document.forms[0];
+form.onsubmit = function(e) {
+    e.preventDefault();
+    let xhr = new XMLHttpRequest();
+    let formData = new FormData(form);
+    xhr.open('POST', 'http://localhost:8080/?method=createTicket');
+    xhr.send(formData);
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            console.log('Работает!');
+        }
+    }
+}
--- src/js/server.js
+++ src/js/server.js
@@ -1,30 +1,41 @@
 const http = require('http');
+const koaBody = require('koa-body');
 const Koa = require('koa');
 const app = new Koa();
-
-const port = 7070;
 
 const tickets = [
     {},
     {}
 ];
 
+app.use(koaBody({
+    urlencoded: true,
+    multipart: true,
+}))
+
 app.use(async ctx => {
+    ctx.response.set({
+        'Access-Control-Allow-Origin': '*',
+    })
     const { method } = ctx.request.querystring;
 
     switch (method) {
         case 'allTickets':
             ctx.response.body = tickets;
-        case `ticketById&id=${id}`:
+            return;
+        case `ticketById`:
+            const { id } = ctx.request.query;
             ctx.response.body = tickets[id];
+            return;
         case 'createTicket':
             ctx.response.body = 'Тикет создан';
-            tickets.push()
+            console.log(ctx.request.query);
+            ctx.response.status = 200;
         default:
             ctx.response.status = 404;
             return;
     }
 });
 
-const server = http.createServer(app.callback()).listen(7070);
+const server = http.createServer(app.callback()).listen(8080);
 
